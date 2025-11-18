97714a64e81e7f5057a72c05ebfa322e72f358e5
Kelly Loo <kyloo@mancos.local>
2025-11-18 13:27:21 -0800

    modified code

--- completeflights.sh
+++ completeflights.sh
@@ -12,15 +12,14 @@ OUTPUT:
 
 CONVENTION
 
+# # change the following
 # local path to google drive 2025 NHOP folder
 gdrive_path="$HOME/Library/CloudStorage/GoogleDrive-kloo@ucsd.edu/.shortcut-targets-by-id/1_L7QmkPn1WUVfeTuCeB04HKaOsed07O2/1 - 2025 Data/NHOP"
 # local path to agsd's shared workspace
 dropbox_path="$HOME/agsd Dropbox/agsd's shared workspace/data_temp/tc25_usaf_aro/1_complete"
-# local path to your downloads folder
-download_path="$HOME/Downloads/"
-# local path to your desktop
-desktop_path="$HOME/Desktop/"
 
+download_path="$HOME/Downloads/"
+desktop_path="$HOME/Desktop/"
 
 # get file name input from user
 echo -n "Enter flight date (YYYY-MM-DD): "
@@ -39,14 +38,14 @@ read -r mission
 month="${date:5:2}"
 
 # check if flight exists
+filename="$date"_"$storm"_"$tail"
 cd "$gdrive_path"/"$month"*
-
 if [ -d *"$mission"* ]; then
     # flight data has been upload, time to create directory to store data in agsd dropbox
     echo "Flight data exists."
     # go to tc25 usaf folder in dropbox
     cd "$dropbox_path"
-    mkdir "$date"_"$storm"_"$tail"
+    mkdir $filename
 else
     echo "Flight data has not been uploaded yet."
     exit
@@ -56,33 +55,28 @@ fi
 
 # copy .SUM file
 cd "$gdrive_path"/"$month"*/*"$mission"*
-
 if [ -e *.SUM ]; then
     echo ".SUM file exists."
-    cp *.SUM "$dropbox_path"/"$date"_"$storm"_"$tail"
+    cp *.SUM "$dropbox_path/$filename"
 else
     echo ".SUM file doesn't exist, flight data is incomplete."
 fi
 
-# retrieve ARO file
+# copy ARO file
 cd "$gdrive_path"/"$month"*/*"$mission"*
-
 arofile=$(find . -name "*.25*")
-
 if [[ -z "${arofile[@]}" ]]; then # checks if $arofile string is empty
     echo "ARO file missing, data incomplete."
 else
     # copy all ARO files (there will be 2 files if the flight occured past 00z)
-    cp $arofile "$dropbox_path"/"$date"_"$storm"_"$tail"
-    
+    cp $arofile "$dropbox_path/$filename"
     # rename ARO file
-    cd "$dropbox_path"/"$date"_"$storm"_"$tail"
+    cd "$dropbox_path/$filename"
     for aro in $arofile; do
         if [[ "$aro" == *".25_.A" ]]; then
             mv "$aro" "${aro/.A/}"
         fi
     done
-
     # prints out the name of the fixed ARO file (good for verification)
     finalarofile=$(find . -name "*.25_")
     for final in $finalarofile; do
@@ -94,22 +88,19 @@ fi
 cd "$gdrive_path"/"$month"*/*"$mission"*
 if [ -e *.tgz ]; then
     echo ".tgz file exists."
-
-    cp *.tgz ~/Desktop/launchpad
-    cd $HOME/Desktop/launchpad
+    mkdir "$dropbox_path/$filename"/tgz_folder
+    cp *.tgz "$dropbox_path/$filename/tgz_folder"
+    cd "$dropbox_path/$filename/tgz_folder"
     tar -xzf *.tgz
-
     for file in *.txt; do
         mv "$file" "${file/\Data\\\\/}"
     done
-
-    cp *.txt "$dropbox_path"/"$date"_"$storm"_"$tail"
-    rm -f *
+    mv *.txt ..
 else
     echo ".tgz file doesn't exist, flight data is incomplete."
 fi
 
-echo "File name is "$date"_"$storm"_"$tail""
+echo "File name is $filename"
 # cd "$dropbox_path"
 # open "$date"_"$storm"_"$tail"
 # open "$gdrive_path"/"$month"*/*"$mission"*
